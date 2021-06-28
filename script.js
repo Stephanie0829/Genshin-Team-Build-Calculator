3b13b75c33a224c30dc049c67bc0a238bd47fab7
Stephanie0829 <72533858+Stephanie0829@users.noreply.github.com>
2021-06-28 01:02:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-sdfsdfsd
+function calcscore() {
+    var score = 0;
+
+
+    // call function to change rating html
+    calcRating(score);
+}
+
+function calcRating(score){
+    var starimg = document.getElementById("rating-img");
+    starimg.style.display = "block";
+    // divide score into 5 sections (consider a score out of 100 points)
+    if(score >=0 && score <= 19){ //1 star
+        starimg.src = "media/Utilities/1star.png";
+    } else if(score >=20 && score <=39) {//2 star
+        starimg.src = "media/Utilities/2stars.png";
+    } else if(score >=40 && score <=59) {//3 star
+        starimg.src = "media/Utilities/3stars.png";
+    } else if(score >=60 && score <=79) {//4 star
+        starimg.src = "media/Utilities/4stars.png";
+    } else {//5 star
+        starimg.src = "media/Utilities/5stars.png";
+    }
+}
